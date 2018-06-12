2babfe41d53200d08fa306b2ffe2607932aac711
Lawrence Whiteside <law@cinemasetfree.com>
2018-06-11 17:29:34 -0700

    quick fix

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -1,71 +1,72 @@
-import React from 'react'
-import {Field, reduxForm, focus} from 'redux-form'
-import {Link} from 'react-router-dom'
+import React from "react"
+import { Field, reduxForm, focus } from "redux-form"
+import { Link } from "react-router-dom"
 
-import Pitch from './pitch'
-import {login} from '../../actions/auth-actions'
-import {required, nonEmpty} from '../../validators'
-import Input from './input'
+import Pitch from "./pitch"
+import { login } from "../../actions/auth-actions"
+import { required, nonEmpty } from "../../validators"
+import Input from "./input"
 
-import '../../styles/login/landing.css'
+import "../../styles/login/landing.css"
 
 export class LoginForm extends React.Component {
   onSubmit(values) {
-    return this.props.dispatch(login(values.username, values.password))
+    this.props.dispatch(login(values.user, values.pass))
   }
 
   render() {
-    let error;
+    let error
     if (this.props.error) {
       error = (
-        <div className='form-error' aria-live='polite'>
+        <div className="form-error" aria-live="polite">
           {this.props.error}
         </div>
       )
     }
     return (
-    <div className='login-page landing'>
-      <Pitch />
-      <form
-        className='login-form form'
-        onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-        {error}
-        <div className='field'>
-          <label htmlFor='user'>Username</label>
-          <Field
-            component={Input}
-            type='text'
-            name='user'
-            id='user'
-            validate={[required, nonEmpty]}
-          />
+      <div className="login-page landing">
+        <Pitch />
+        <form
+          className="login-form form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+        >
+          {error}
+          <div className="field">
+            <label htmlFor="user">Username</label>
+            <Field
+              component={Input}
+              type="text"
+              name="user"
+              id="user"
+              validate={[required, nonEmpty]}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="pass">Password</label>
+            <Field
+              component={Input}
+              type="password"
+              name="pass"
+              id="pass"
+              validate={[required, nonEmpty]}
+            />
+          </div>
+          <div className="btn-container">
+            <button disabled={this.props.pristine || this.props.submitting}>
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="footer-li">
+          <p>New Here?</p>
+          <Link to="/signup">Signup!</Link>
         </div>
-        <div className='field'>
-          <label htmlFor='pass'>Password</label>
-          <Field
-            component={Input}
-            type='password'
-            name='pass'
-            id='pass'
-            validate={[required, nonEmpty]}
-          />
-        </div>
-        <div className='btn-container'>
-          <button disabled={this.props.pristine || this.props.submitting}>
-            Login
-          </button>
-        </div>
-      </form>
-      <div className='footer-li'>
-        <p>New Here?</p>
-        <Link to='/signup'>Signup!</Link>
       </div>
-    </div>
     )
   }
 }
 
 export default reduxForm({
-  form: 'login',
-  onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+  form: "login",
+  onSubmitFail: (errors, dispatch) => dispatch(focus("login", "username"))
 })(LoginForm)
--- src/components/login/signup-form.js
+++ src/components/login/signup-form.js
@@ -1,91 +1,94 @@
-import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
+import React from "react"
+import { Field, reduxForm, focus } from "redux-form"
 
-import Pitch from './pitch'
-import {registerUser} from '../../actions/user-actions';
-import {login} from '../../actions/auth-actions';
-import Input from './input';
-import {Link} from 'react-router-dom'
-import {required, nonEmpty, matches, length, isTrimmed} from '../../validators';
+import Pitch from "./pitch"
+import { registerUser } from "../../actions/user-actions"
+import { login } from "../../actions/auth-actions"
+import Input from "./input"
+import { Link } from "react-router-dom"
+import {
+  required,
+  nonEmpty,
+  matches,
+  length,
+  isTrimmed
+} from "../../validators"
 
-import '../../styles/login/landing.css'
+import "../../styles/login/landing.css"
 
-const passwordLength = length({min: 10, max: 72});
-const matchesPassword = matches('password');
-
+const passwordLength = length({ min: 10, max: 72 })
+const matchesPassword = matches("password")
 
 export class SignupForm extends React.Component {
-    onSubmit(values) {
-        const {username, password, firstName, lastName} = values;
-        const user = {username, password, firstName, lastName};
-        return this.props
-            .dispatch(registerUser(user))
-            .then(() => this.props.dispatch(login(username, password)));
-    }
+  onSubmit(values) {
+    const { username, password, firstName, lastName } = values
+    const user = { username, password, firstName, lastName }
+    this.props.dispatch(registerUser(user))
+  }
 
-    render() {
-        return (
-          <div className='signup-page landing'>
-            <Pitch />
-            <form
-                className="signup-form form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                <div className='field'>
-                  <label htmlFor="firstName">First name</label>
-                  <Field component={Input} type="text" name="firstName" />
-                </div>
-                <div className='field'>
-                  <label htmlFor="lastName">Last name</label>
-                  <Field component={Input} type="text" name="lastName" />
-                </div>
-                <div className='field'>
-                  <label htmlFor="username">Username</label>
-                  <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    validate={[required, nonEmpty, isTrimmed]}
-                  />
-                </div>
-                <div className='field'>
-                  <label htmlFor="password">Password</label>
-                  <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    validate={[required, passwordLength, isTrimmed]}
-                  />
-                </div>
-                <div className='field'>
-                  <label htmlFor="passwordConfirm">Confirm password</label>
-                  <Field
-                    component={Input}
-                    type="password"
-                    name="passwordConfirm"
-                    validate={[required, nonEmpty, matchesPassword]}
-                  />
-                </div>
-                <div className='btn-container'>
-                  <button
-                      type="submit"
-                      disabled={this.props.pristine || this.props.submitting}>
-                      Register
-                    </button>
-                  </div>
-            </form>
-            <div className='footer-su'>
-              <p>Have an Account?</p>
-              <Link to='/'>Login!</Link>
-            </div>
+  render() {
+    return (
+      <div className="signup-page landing">
+        <Pitch />
+        <form
+          className="signup-form form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+        >
+          <div className="field">
+            <label htmlFor="firstName">First name</label>
+            <Field component={Input} type="text" name="firstName" />
           </div>
-        );
-    }
+          <div className="field">
+            <label htmlFor="lastName">Last name</label>
+            <Field component={Input} type="text" name="lastName" />
+          </div>
+          <div className="field">
+            <label htmlFor="username">Username</label>
+            <Field
+              component={Input}
+              type="text"
+              name="username"
+              validate={[required, nonEmpty, isTrimmed]}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <Field
+              component={Input}
+              type="password"
+              name="password"
+              validate={[required, passwordLength, isTrimmed]}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="passwordConfirm">Confirm password</label>
+            <Field
+              component={Input}
+              type="password"
+              name="passwordConfirm"
+              validate={[required, nonEmpty, matchesPassword]}
+            />
+          </div>
+          <div className="btn-container">
+            <button
+              type="submit"
+              disabled={this.props.pristine || this.props.submitting}
+            >
+              Register
+            </button>
+          </div>
+        </form>
+        <div className="footer-su">
+          <p>Have an Account?</p>
+          <Link to="/">Login!</Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default reduxForm({
-    form: 'registration',
-    onSubmitFail: (errors, dispatch) =>
-        dispatch(focus('registration', Object.keys(errors)[0]))
-})(SignupForm);
+  form: "registration",
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus("registration", Object.keys(errors)[0]))
+})(SignupForm)
