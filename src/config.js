656aab37cb7fb74d8dc250756ace3ec789449802
Liz Zitnik <lizbiz@Lizs-MacBook-Pro.local>
2018-07-07 13:38:20 -0700

    linked server

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'https://home-api11.herokuapp.com';
+export const API_BASE_URL = 'https://home-server11.herokuapp.com'
+
+
+
+    // process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001';
